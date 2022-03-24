b5fb7d645c0167ad67e7afc010e7138e97e93d92
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-03-24 17:41:30 +0200

    cant fix the graph

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ const endpoint = "victor1";
 
 const enterBtn = document.querySelector("#enter");
 const cardFrame = document.querySelector("#cardFrame");
-const cards = document.querySelectorAll(".card")
 
 const ctx = document.getElementById('chart');
 let myChart = new Chart(ctx, {
@@ -17,23 +16,33 @@ let myChart = new Chart(ctx, {
         labels: ['Protein', 'Carbs', 'Fat'],
         datasets: [{
             label: 'g',
-            data: [10, 10, 10],
+            data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
- 
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
-
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
         }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
     }
-
 });
 
 const API = new FetchWrapper(url);
@@ -59,6 +68,8 @@ const getData  = () => {
             </div>
         </div> 
             `)
+
+            console.log(item.fields.foodName.stringValue, item.fields.protein.integerValue, item.fields.fat.integerValue, item.fields.carbs.integerValue);
         });
     });
 }
@@ -92,16 +103,12 @@ const postData = () => {
             },
         }
 
-        //API.post(endpoint, body);
-        snackbar.show('${foodName} added');
-        updateChart();
-        getData(protein, carbs, fat);
-    }
-}
+        API.post(endpoint, body);
+        snackbar.show('${foodName} added')
+        getData()
 
-const updateChart = (protein, carbs, fat) => {
-    myChart?.destroy();
-    myChart.datasets.data = [protein, carbs, fat];
+    }
+
 }
 
 snackbar.show("Systems online!");
